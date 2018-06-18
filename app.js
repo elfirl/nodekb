4447ad78af0670ada3ce08125c7db421c3859305
Mike Bilter <mike@Mikes-MacBook-Pro.local>
2018-06-18 14:07:06 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+})
+
+// Check for DB errors
+db.on('error', (err) => {
+    console.log(err);
+})
+
+// Init app
+const app = express();
+
+// Bring in models
+let Article = require('./models/articles');
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// Parse application/json
+app.use(bodyParser.json())
+
+// Home Route
+app.get('/', (req, res) => {
+    Article.find({}, (err, articles) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
+        }
+        
+    })  
+});
+
+// Add Route
+app.get('/articles/add', (req, res) => {
+    res.render('add_article', {
+        title: 'Add Article'
+    });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', (req, res) => {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save((err) => {
+        if(err) {
+            console.log(err)
+            return;
+        } else {
+            res.redirect('/');
+        }
+    })
+});
+
+// Start Server
+app.listen(3000, () => {
+    console.log('Server started on port 3000 ...')
+});
